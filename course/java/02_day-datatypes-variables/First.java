14b0632718f582444f8536d5022d4ef9257e26a0
Dhiraj Rajput <code.dhiraj18@gmail.com>
2026-02-22 14:33:50 +0530

    done

---
+++ course/java/02_day-datatypes-variables/First.java
@@ -0,0 +1,24 @@
+
+
+class DatatypesAndVariables {
+    // Datatypes and variables 
+    String Name = "Dhiraj rajput";
+    int Age = 22;
+    char Letter = 'd';
+    double val = 324.343686868678987987d;
+    float values = 34.00f;
+    boolean type = true;
+    
+    // constatn variable 
+    final int minPerHour = 60;
+    // minPerHour = 44;  // Can not assing new value to the constant variable
+}
+
+public class First {
+    public static void main (String [] argu){
+        Sarent obj = new Sarent();
+        System.out.println(obj.Name + " " + obj.Age + " " + obj.Letter + " " + obj.val + " " + obj.values);
+        System.out.println( "The sum of age and val are : "+obj.Age + obj.val);
+    }
+}
+
