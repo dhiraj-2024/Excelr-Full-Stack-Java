0327cda45a76de0b254c8951374988ad5ddd7fa0
Dhiraj Rajput <code.dhiraj18@gmail.com>
2026-02-24 17:22:40 +0530

    loops compleated

---
+++ course/java/05_day-loops/Loops.java
@@ -0,0 +1,23 @@
+public class Loops {
+    public static void main(String[] argu) {
+        System.out.println("hwllo boys");
+
+        // for loop
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        // while loop
+        int j = 50;
+        while (j <= 100) {
+            System.out.println(j);
+            j++;
+        }
+        // do while loop
+        int k = 0;
+        do {
+            System.out.println(k);
+            k++;
+        } while (k <= 10);
+    }
+}
