76b93531aa36c91b8c3cc4721a561154eadb1262
Dhiraj Rajput <code.dhiraj18@gmail.com>
2026-02-27 21:54:09 +0530

    day 16

---
+++ fullstack-java-daily-practice/16_day/index.js
@@ -0,0 +1,94 @@
+console.log("hello world");
+
+var name = "dhiraj";
+let lastName = "rajput";
+const age = 20;
+
+console.log(`hello my name is ${name} ${lastName} and my age is ${age} .`)
+
+
+name = "saii";
+lastName = "lala"
+// age = 22;
+console.log(name)
+console.log(lastName)
+console.log(age);
+
+//Global Scope
+
+/*
+ - A variable declared outside any function or block
+ - Accessible everywhere in your code
+*/
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+function show() {
+    console.log(a); // ✅ 10
+    console.log(b); // ✅ 20
+    console.log(c); // ✅ 30
+}
+show();
+
+
+// Function Scope (Only for var)
+// - Variables declared with var inside a function are only accessible within that function.
+function testVarScope() {
+    var x = 5;
+    console.log(x); // ✅ 5
+}
+testVarScope();
+// console.log(x); ❌ ReferenceError. not allowed for var
+
+
+// Block Scope (Only for let and const)
+/*
+ - A block is any code inside {} (like if, for, while, functions)
+ - let and const are block scoped — only accessible inside that block
+*/
+
+{
+    let name = "Dhiraj";
+    const city = "Delhi";
+    console.log(name); // ✅ Dhiraj
+    console.log(city); // ✅ Delhi
+}
+
+//console.log(name);  //❌ ReferenceError
+//console.log(city);  //❌ ReferenceError
+
+
+let array = [
+    {
+        name: "dhiraj",
+        age: 22,
+    },
+    {
+        name: "saii",
+        age:21,
+    },
+]
+
+console.log(array[1].age);
+
+
+
+class person{
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    check() {
+        console.log(`hello my name is ${this.name} and age is ${this.age}`);
+    }
+}
+
+const obj = new person("dhiraj", 22)
+obj.check()
+
+let value = null;
+let value2 = undefined;
+
+console.log(value & value2)
