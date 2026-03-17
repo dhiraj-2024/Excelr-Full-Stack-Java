90bf5aa452eb1cbfcb403fc7298332e92f427248
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-03-17 13:05:58 +0000

    day 16 assignment done

---
+++ assignments/01_html-css-js/16_day/script.js
@@ -0,0 +1,43 @@
+// Q1. 
+
+let x = 100;
+let y = 200;
+const result = x + y;
+
+let z = result;
+console.log(z)
+
+
+// Q2.
+
+let a = 10;
+const square = a*a;
+
+let b = square;
+
+console.log(b)
+
+// Q3.
+let firstName = "Excelr"
+let lastName = "EduTech"
+let fullName = firstName + " " + lastName;
+
+console.log(fullName)
+
+// Q4. 
+
+let cource = "fullstack";
+let welcomeMsg = `hello welcome to your ${cource} cource`
+
+console.log(welcomeMsg)
+
+
+// Q5. 
+ 2 > 4 ? console.log("hello") : console.log("bye");
+
+
+//  Q6. 
+
+let num = 7;
+
+num % 2 == 0 ? console.log("number is even") : console.log("number is odd")
