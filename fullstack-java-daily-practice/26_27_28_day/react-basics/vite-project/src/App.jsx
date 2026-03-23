f16b6ae3469eb357e7a471b68c40163e6d3fc4a2
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-03-23 08:17:23 +0000

    react basic project setup day 26

---
+++ fullstack-java-daily-practice/26_27_28_day/react-basics/vite-project/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+function App() {
+
+  const users = [
+  { id: 1, name: 'John', age: 30 },
+  { id: 2, name: 'Jane', age: 25 },
+  { id: 3, name: 'Bob', age: 35 }
+];
+
+const fruitlist = ['apple', 'banana', 'cherry'];
+
+  return (
+    <>
+      <ul>
+        {users.map(user => 
+          <li key={user.id}>
+            {user.name} is {user.age} years old
+          </li>
+        )}
+      </ul>
+      <ul>
+        {
+          fruitlist.map(fruit =>
+            <li key={fruit}> Name : {fruit}</li>
+          )
+        }
+      </ul>
+
+    </>
+  )
+}
+
+export default App
