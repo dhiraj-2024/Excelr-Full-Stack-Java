bacad243b7f17922a08b978d39f6c14aa39e6999
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-04-06 19:40:50 +0530

    abstraction - interface

---
+++ fullstack-java-daily-practice/45_day/abstraction/Animal.java
@@ -0,0 +1,31 @@
+abstract class Dog {
+
+    String name = "Sherruu";
+    int age = 5;
+
+    abstract void fun1();
+
+    Dog() {
+        System.out.println("this is the Dog class constructor");
+    }
+}
+
+class AnotherDog extends Dog {
+    void fun1() {
+        System.out.println("this is the another dog function");
+    }
+}
+
+public class Animal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        // Dog d1 = new Dog();    // it gives error
+        // System.out.println(d1.name);
+
+        AnotherDog d2 = new AnotherDog();
+        System.out.println(d2.name);
+        d2.fun1();
+    }
+    
+}
