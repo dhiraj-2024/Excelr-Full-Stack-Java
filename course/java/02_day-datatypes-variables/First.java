783ba09d03961198694f7abce89bf9c5a0cabbc2
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-04-10 15:26:17 +0530

    full java lecture 3 AC

--- course/java/02_day-datatypes-variables/First.java
+++ course/java/02_day-datatypes-variables/First.java
@@ -2,12 +2,22 @@
 
 class DatatypesAndVariables {
     // Datatypes and variables 
-    String Name = "Dhiraj rajput";
-    int Age = 22;
-    char Letter = 'd';
+    // INTEGER 
+    byte num = 34;
+    short num2 = 2394;
+    int num3 = 2223944;
+    long num4 = 2338093298L;
+
+    int bynary = 0b1011001101; // bynary number use 0b
+    int octal = 07; // use 0 befor number 
+    int hex = 0X5A;
+
+    // REAL NUMBERS     
+    float values = 34.3420f;
     double val = 324.343686868678987987d;
-    float values = 34.00f;
     boolean type = true;
+    char Letter = 'd';
+    String Name = "Dhiraj rajput";
     
     // constatn variable 
     final int minPerHour = 60;
@@ -17,8 +27,11 @@ class DatatypesAndVariables {
 public class First {
     public static void main (String [] argu){
         DatatypesAndVariables obj = new DatatypesAndVariables();
-        System.out.println(obj.Name + " " + obj.Age + " " + obj.Letter + " " + obj.val + " " + obj.values);
-        System.out.println( "The sum of age and val are : "+obj.Age + obj.val);
+        System.out.println(obj.num + " " + obj.num2 + " " + obj.num3 + " " + obj.num4 + " " + obj.bynary + " "
+                + obj.octal + " " + obj.hex);
+        System.out.println(obj.values);
+        System.out.println(obj.val);
+        System.out.println( "The sum of age and val are : "+ obj.val);
     }
 }
 
