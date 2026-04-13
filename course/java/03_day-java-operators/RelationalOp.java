cabeeafab481a48054730c6aa1cfa489e8cbd756
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-04-13 17:48:11 +0530

    Relational Operator done

---
+++ course/java/03_day-java-operators/RelationalOp.java
@@ -0,0 +1,19 @@
+public class RelationalOp {
+    
+    public static void main(String[] args) {
+
+        // Relational Operators == ,!= , < , > , <= , >=
+    int    a = 5;
+    int   b = 4;
+    int assignValue = a = b;
+    
+    // boolean check = a == b;
+    
+    boolean checkNEqual = a != b;
+    System.out.println(checkNEqual);
+    //  System.out.println(check);
+    //  System.out.println(b);
+    //  System.out.println(a);
+    System.out.println(assignValue);
+    }
+}
