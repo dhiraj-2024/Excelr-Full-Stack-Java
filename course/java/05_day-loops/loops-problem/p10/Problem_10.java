b2102e96a30ef840b17e27820cd9db0750f43dc2
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-04-30 15:02:44 +0530

    loops problem 10 : print the factorial of given number

---
+++ course/java/05_day-loops/loops-problem/p10/Problem_10.java
@@ -0,0 +1,17 @@
+// CALCULATE AND PRINT THE FACTORIAL OF THE GIVEN NUMBER
+
+public class Problem_10 {
+    public static void main (String [] args)
+    {
+        int i = 1;
+        int n = 10;
+        int fsum = 1;
+        while (i <= n) {
+            fsum *= i;
+            i++;
+
+        }
+        System.out.println( fsum);
+    }
+
+}
