286d225704a1ade15eb7a845631a0bb0505ce216
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-04-30 14:06:51 +0530

    loops problem 5 done multiplication table

---
+++ course/java/05_day-loops/loops-problem/p5/Problem_5.java
@@ -0,0 +1,14 @@
+// PRINT THE MULTIPLICATION TABLE FROM THE GIVEN NUMBER FROM N * 1 TO N *10
+
+public class Problem_5 {
+
+    public static void main(String[] args) {
+        
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(21*i);
+            i++;
+        }
+    }
+    
+}
