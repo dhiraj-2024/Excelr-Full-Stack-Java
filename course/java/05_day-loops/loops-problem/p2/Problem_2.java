86b349bc94a5fc3becb8b318a5f062ac54735459
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-04-30 13:56:27 +0530

    problem 2 loop done

---
+++ course/java/05_day-loops/loops-problem/p2/Problem_2.java
@@ -0,0 +1,13 @@
+// PRINT NUMBERS FROM 10 TO 1 IN REVERCE ORDER
+
+public class Problem_2 {
+    public static void main(String[] args) {
+        
+        int i = 10;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+
+        }
+    }
+}
