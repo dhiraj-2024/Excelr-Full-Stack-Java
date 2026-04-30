b59ac09bbfe8afed88d1e5ca9b7e0b3e2cf01b42
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-04-30 13:54:01 +0530

    loops problem 1 done

---
+++ course/java/05_day-loops/loops-problem/p1/Problem_1.java
@@ -0,0 +1,11 @@
+// PRINT ALL NUMBERS FROM 1 TO 10 USING WHILE LOOPS 
+public class Problem_1 {
+    public static void main(String[] args) {
+        
+        int i = 1;
+        while (i<=10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
