038eb93a9235cf54464a7a44e377bacbe31d9c07
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-05-04 20:22:32 +0530

    problem 18

---
+++ course/java/05_day-loops/loops-problem/p18/Problem_18.java
@@ -0,0 +1,11 @@
+// CHECK WHETHER THE GIVEN NUMBER IS PRIME OR NOT 
+
+public class Problem_18 {
+    public static void main(String[] args) {
+        int num = 5;
+        int i = 2;
+        while (i <= num) {
+            
+        }
+    }
+}
