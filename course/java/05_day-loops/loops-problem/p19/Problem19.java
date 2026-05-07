1791876644217f1e3171d51f7474bea8c79b6ad2
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-05-07 22:11:36 +0530

    pattern Printing using for loop

--- course/java/05_day-loops/loops-problem/p19/Problem19.java
+++ course/java/05_day-loops/loops-problem/p19/Problem19.java
@@ -1,4 +1,12 @@
 
 public class Problem19 {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i >= 0) {
+            System.out.println(i);
+            i--;
+            
+        }
+    }
     
 }
