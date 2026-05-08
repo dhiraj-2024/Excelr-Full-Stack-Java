b4dc0335fadb4d7804081b01510390d54d3eaf6b
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-05-08 19:27:08 +0530

    increasing decreasing trangle pattern printing

--- course/java/05_day-loops/loops-problem/ForLoop/p3/Problem3.java
+++ course/java/05_day-loops/loops-problem/ForLoop/p3/Problem3.java
@@ -1,13 +1,37 @@
-// Decreasing Trangle program 
+// INCREASING Trangle program 
 
 public class Problem3 {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j <= n; j++) {
+        // INCREASING Trangle program
+        int num = 5;
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("$ ");
+            }
+            System.out.println();
+        }
+
+        // DECREASING Trangle program
+
+        int row = 5;
+        for (int k = 1; k <= row; k++) {
+            for (int l = k; l <= row; l++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
+
+   
+
 }
+
+
+/*
+k = 1   l = k = 1    * * * * * 
+k = 2   l = k = 2    * * * * 
+k = 3   l = k = 3    * * * 
+k = 4   l = k = 4    * *
+k = 5   l = k = 5    *
+
+*/
