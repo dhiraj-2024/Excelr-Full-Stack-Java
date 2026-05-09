270b149e77fb00ff1ccb6785fe3847b7aea676ee
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-05-09 23:17:04 +0530

    day 42 java assignment done

---
+++ assignments/02_java-assignments-daily/42_day/Q1.java
@@ -0,0 +1,25 @@
+// WAJP  that declare all premetive datatype variable and print them 
+
+public class Q1 {
+    public static void main(String[] args) {
+        // premetive datatypes : 
+        byte n1 = 22;
+        System.out.println(n1);
+        short n2 = 222;
+        System.out.println(n2);
+        int n3 = 22222;
+        System.out.println(n3);
+        long n4 = 222343234;
+        System.out.println(n4);
+        float n5 = 22.34f;
+        System.out.println(n5);
+        double n6 = 22324.23443;
+        System.out.println(n6);
+        char alpha = 'a';
+        System.out.println(alpha);
+        boolean isCheck = true;
+        System.out.println(isCheck);
+
+
+    }
+}
