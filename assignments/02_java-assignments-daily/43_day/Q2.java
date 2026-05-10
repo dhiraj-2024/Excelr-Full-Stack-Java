8d5c4ec280529240cd21b4c6b632d93c3627c293
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-05-10 11:07:04 +0530

    q2 done

---
+++ assignments/02_java-assignments-daily/43_day/Q2.java
@@ -0,0 +1,45 @@
+// Identifiers in java valid and invalid name of variable , methods and class name 
+
+public class Q2 {
+    public static void main(String[] args) {
+        int value_name;
+        int _value;
+        int $value;
+        int Value;
+
+    }
+
+    void Fun() {
+
+    }
+
+    void $Hello() {
+
+    }
+
+    void _Helloo() {
+
+    }
+
+    void sdfaj() {
+
+    }
+
+}
+
+class Hello {
+
+}
+
+class $Hello {
+
+}
+
+class _Hello {
+
+}
+
+class hellol {
+
+}
+
