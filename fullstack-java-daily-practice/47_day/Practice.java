c6e8c26f7433756de6cbd0af37b3994d2875c0d4
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-07-24 18:39:18 +0530

    revise java

---
+++ fullstack-java-daily-practice/47_day/Practice.java
@@ -0,0 +1,17 @@
+class Practice {
+
+    
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println(sum);
+        int p = 88;
+        System.out.println(p);
+
+    }
+
+
+
+}
