eecd471eb563eee08c9a4069e31e9c2cb198b9e6
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-07-25 12:44:29 +0530

    cunstructor practice

--- fullstack-java-daily-practice/47_day/Students.java
+++ fullstack-java-daily-practice/47_day/Students.java
@@ -5,6 +5,19 @@ public class Students {
     String s_name;
     int s_age;
 
+    Students() {
+        s_name = "unknon";
+        s_age = 0;
+    }
+
+    Students(String s_name) {
+        this.s_name = s_name;
+    }
+
+    Students(Students obj) {
+        this.s_age = obj.s_age;
+        this.s_name = obj.s_name;
+    }
     void display() {
         System.out.println(college_name + " " + s_name + " " + s_age);
     }
@@ -16,8 +29,15 @@ public class Students {
         Students s2 = new Students();
         s2.s_name = "pooja";
         s2.s_age = 21;
-
         s1.display();
         s2.display();
+
+        Students s3 = new Students("pramod");
+        s3.s_age = 23;
+        s3.display();
+
+        Students obj = new Students(s1);
+        obj.display();
+
     }
 }
