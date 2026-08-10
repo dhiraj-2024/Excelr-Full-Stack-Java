92d453531122c8bfca24e1b9afe8233e8e47ff9c
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-08-10 22:21:18 +0530

    palindrom of number

---
+++ DSA/Numbers/01_Palindrom/Main.java
@@ -0,0 +1,20 @@
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//         int num = 434;
+//         int temp = num;
+//         int rev = 0;
+//         while (num != 0) {
+//             int digit = num % 10;
+//             rev = rev * 10 + digit;
+//             num = num / 10;
+//         }
+//         if (temp == rev) {
+//             System.out.println("palindrom");
+//         } else {
+//             System.out.println("not palindrom");
+//         }
+//     }
+// }
+
