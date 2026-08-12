15b4115b14cb1e7cb0d786af356fa7ecdba28984
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-08-12 13:19:25 +0530

    pattern 5

---
+++ DSA/Patterns/05_Pattern/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+    public static void main(String[] args) {
+        Pattern05(3);
+    }
+
+    static void Pattern05(int n) {
+        for (int i = 1; i <= 2 * n; i++) {
+            int totalColumn = i < n ? i : 2 * n - i;
+            for (int j = 1; j <= totalColumn; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
