b27e3c3039014fc71cd33fe244836da7a825ca64
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-08-13 15:55:28 +0530

    nested sum

---
+++ DSA/Arrays/28_NestedSumArrays/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] nums = {
+                { 1, 2, 3 },
+                { 4, 5 },
+                { 6, 7, 8 }
+        };
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                sum += nums[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
