71172fffc82babc6023db5b5fdac7840bfdc2435
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2026-08-20 19:07:46 +0530

    insersion sort

---
+++ DSA/Arrays/43_InsersionSort/Main.java
@@ -0,0 +1,19 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = { 50, 30, 10, 40, 20, };
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
